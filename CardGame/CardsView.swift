0343f657523056b75db4ef6ad0ae18157e3b3c74
Ali Can Kayaaslan <ackayaaslan@gmail.com>
2023-03-01 13:54:05 +0300

    View Added

---
+++ CardGame/CardsView.swift
@@ -0,0 +1,20 @@
+//
+//  CardsView.swift
+//  CardGame
+//
+//  Created by Ali Can Kayaaslan on 28.02.2023.
+//
+
+import SwiftUI
+
+struct CardsView: View {
+    var body: some View {
+        Text("Hello, ACKKKK!")
+    }
+}
+
+struct CardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardsView()
+    }
+}
