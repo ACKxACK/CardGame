530db3463e810f599139814b6b0b106f192017fb
Ali Can Kayaaslan <ackayaaslan@gmail.com>
2023-03-01 17:09:51 +0300

    Background and logo added

--- CardGame/ContentView.swift
+++ CardGame/ContentView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("ACK!")
+        ZStack {
+            Image("background-cloth")
+            VStack{
+                Image("logo")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("CARD GAME!")
+            }
+            
         }
         .padding()
     }
