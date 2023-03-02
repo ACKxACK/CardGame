218c68c72e12298f74fa93ffbc1a2baf83f91850
Ali Can Kayaaslan <ackayaaslan@gmail.com>
2023-03-02 15:15:19 +0300

    Assets added, modified Content View

--- CardGame/ContentView.swift
+++ CardGame/ContentView.swift
@@ -10,12 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Image("background-cloth")
+            Image("background-plain")
+                .ignoresSafeArea()
             VStack{
                 Image("logo")
                     .imageScale(.large)
                     .foregroundColor(.accentColor)
-                Text("CARD GAME!")
+                    .padding(.bottom, 50)
+                HStack {
+                    Spacer()
+                    Image("back")
+                    Spacer()
+                    Image("back")
+                    Spacer()
+                }
+                Button {
+                    //TO DO
+                } label: {
+                    Image("button")
+                        .padding(30)
+                }
+
+                HStack {
+                    Spacer()
+                    VStack{
+                        Text("PLAYER")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text("0")
+                            .font(.largeTitle)
+                    }.foregroundColor(.white)
+                        .font(.system(size: 20))
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text("0")
+                            .font(.largeTitle)
+                    }.foregroundColor(.white)
+                        .font(.system(size: 20))
+                    Spacer()
+                }
             }
             
         }
